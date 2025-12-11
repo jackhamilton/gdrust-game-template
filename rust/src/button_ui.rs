1527243ad0289b3e45dd68392dfac7fa099c2573
jackhamilton <jackham800@gmail.com>
2025-12-11 13:32:18 -0700

    Update buttons

---
+++ rust/src/button_ui.rs
@@ -0,0 +1,62 @@
+use opencompose_rs::configs::Button::ButtonConfig;
+use opencompose_rs::configs::view_subtypes::view_alignment::Alignment;
+use opencompose_rs::layout_compositor::Compositor;
+use godot::classes::control::LayoutPreset;
+use opencompose_rs::configs::View::ViewConfig;
+use opencompose_rs::view_builder;
+
+use godot::classes::IControl;
+use godot::classes::Control;
+use godot::{prelude::*};
+
+use crate::gdrust_trinkets::gdui::ast_parser::ASTParser;
+
+#[derive(GodotClass, Debug)]
+#[class(tool, base=Control)]
+pub struct ButtonSampleUI {
+    pub base: Base<Control>
+}
+
+#[godot_api]
+impl IControl for ButtonSampleUI {
+    fn init(base: Base<Control>) -> Self {
+        Self {
+            base
+        }
+    }
+
+    fn enter_tree(&mut self) {
+        let mut dsl_ast = view_builder! {
+            Column {
+                Text("Title")
+                    .font_size(24)
+                Text("Subtitle")
+                    .font_size(12)
+                Row {
+                    Button(action: || {
+                        godot_print!("Pressed");
+                    }) {
+                        Text("Action 1")
+                            .alignment(horizontal: Alignment::Center, vertical: Alignment::Center)
+                    }
+                    .frame(width: 200, height: 30)
+                    Button(action: || {}) {
+                        Text("Action 2")
+                            .alignment(horizontal: Alignment::Center, vertical: Alignment::Center)
+                    }
+                    .frame(width: 200, height: 30)
+                }
+            }
+        };
+        Compositor::layout_ast(&mut dsl_ast);
+        let parser = ASTParser {
+            ast: dsl_ast
+        };
+        let control_children = parser.parse();
+        let mut base_mut = self.base_mut();
+        base_mut.set_anchors_preset(LayoutPreset::CENTER);
+        base_mut.add_child(&control_children);
+        base_mut.set_custom_minimum_size(control_children.get_minimum_size());
+    }
+}
+
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,6 +4,7 @@ struct RustExtension;
 
 pub mod gdrust_trinkets;
 pub mod sample_ui;
+pub mod button_ui;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for RustExtension {}
